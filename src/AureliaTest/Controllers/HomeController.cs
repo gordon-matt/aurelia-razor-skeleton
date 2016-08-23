b96e1befb57f4e6f1029c30c064a11586031b67f
vnmatt <gordon_matt@hotmail.com>
2016-08-24 09:22:31 +0930

    Initial checkin

---
+++ src/AureliaTest/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AureliaTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public IActionResult Host()
+        {
+            return View();
+        }
+
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return PartialView();
+        }
+
+        [Route("app")]
+        public IActionResult App()
+        {
+            return PartialView();
+        }
+
+        [Route("nav-bar")]
+        public IActionResult NavBar()
+        {
+            return PartialView();
+        }
+
+        [Route("flickr")]
+        public IActionResult Flickr()
+        {
+            return PartialView();
+        }
+
+        [Route("test-page")]
+        public IActionResult TestPage()
+        {
+            return PartialView();
+        }
+
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
---
+++ src/AureliaTest/wwwroot/jspm_packages/npm/jquery@3.0.0.js
@@ -0,0 +1,1 @@
+module.exports = require("npm:jquery@3.0.0/dist/jquery.js");
